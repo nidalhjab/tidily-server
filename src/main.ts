5a2f0215f1dd10c14f6894ecc9d4f9a1e4b2e902
nidalhjab <nidal_hjab@hotmail.com>
2022-11-29 02:05:32 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors({ origin: 'https://stately-marigold-a733a6.netlify.app/' });
   await app.listen(4000);
 }
 bootstrap();
